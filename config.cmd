40760b393adc75eed835e60688554a18eacfe99c
Bjørn Dybvik Langfors <bdl@brreg.no>
2019-10-23 10:50:07 +0200

    Fix typos

--- config.cmd
+++ config.cmd
@@ -2,7 +2,7 @@ rem --------- PRODUCTION SETTINGS -----------
 rem The thumbprint for your own enterprise certificate in local machine storage
 set production_certificate_thumbprint=INSERT_PRODUCTION_THUMBPRINT_HERE
 
-rem The client_id that you have provisioned with the tokens you want
+rem The client_id that you have provisioned with the scopes you want
 set production_client_id=INSERT_PRODUCTION_CLIENT_ID_HERE
 
 rem The intended "aud" claim for the access_token
@@ -15,7 +15,7 @@ rem --------- VER2 SETTINGS -----------
 rem The thumbprint for your own enterprise certificate in local machine storage
 set test_certificate_thumbprint=INSERT_TEST_THUMBPRINT_HERE
 
-rem The client_id that you have provisioned with the tokens you want
+rem The client_id that you have provisioned with the scopes you want
 set test_client_id=INSERT_TEST_CLIENT_ID_HERE
 
 rem The intended "aud" claim for the access_token
